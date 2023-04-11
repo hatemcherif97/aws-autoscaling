248d0f6d231c3e2bff0fb68f65acd96d51281161
hatemcherif97 <91384135+hatemcherif97@users.noreply.github.com>
2023-04-11 15:03:05 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,3 +3,6 @@ sudo yum update -y &&
 sudo yum install -y httpd &&
 systemctl start httpd
 systemctl enable httpd
+
+cd /var/www/html
+sudo echo "<html><body><h1>Welcome to my Apache webpage!</h1></body></html>" > /var/www/html/index.html
